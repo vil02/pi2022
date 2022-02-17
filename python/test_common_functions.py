df8814cf91c802519ebc77c31a9f120f6a0b0d59
piotr.idzik <vil02@o2.pl>
2022-02-17 19:43:01 +0100

    refactor: use project_config.py

--- python/test_common_functions.py
+++ python/test_common_functions.py
@@ -1,42 +1,42 @@
 """
-tests of common_functions module
+tests of project_config module
 """
 import pathlib
-import common_functions as cf
+import project_config as pc
 
 
 def test_project_root_dir_exists():
-    """checks result of cf.get_project_root_dir() exists"""
-    assert cf.get_project_root_dir().exists()
+    """checks result of pc.get_project_root_dir() exists"""
+    assert pc.get_project_root_dir().exists()
 
 
 def test_project_root_dir_is_dir():
-    """checks result of cf.get_project_root_dir() is a directory"""
-    assert cf.get_project_root_dir().is_dir()
+    """checks result of pc.get_project_root_dir() is a directory"""
+    assert pc.get_project_root_dir().is_dir()
 
 
 def test_result_of_read_paths_and_names_is_a_dict():
-    """checks if result of cf.read_paths_and_names() is a dictionary"""
-    assert isinstance(cf.read_paths_and_names(), dict)
+    """checks if result of pc.read_paths_and_names() is a dictionary"""
+    assert isinstance(pc.read_paths_and_names(), dict)
 
 
 def test_result_of_read_paths_and_names_is_not_empty():
-    """checks if result of cf.read_paths_and_names() is not empty"""
-    assert cf.read_paths_and_names()
+    """checks if result of pc.read_paths_and_names() is not empty"""
+    assert pc.read_paths_and_names()
 
 
 def test_get_config_parameter():
     """
-    checks if cf.get_config_parameter() uses the result of
-    cf.read_paths_and_names()
+    checks if pc.get_config_parameter() uses the result of
+    pc.read_paths_and_names()
     """
-    for (key, value) in cf.read_paths_and_names().items():
-        assert value == cf.get_config_parameter(key)
+    for (key, value) in pc.read_paths_and_names().items():
+        assert value == pc.get_config_parameter(key)
 
 
 def test_get_config_parameter_tmp_data_folder():
     """
-    checks if cf.get_config_parameter('tmpDataFolder')
+    checks if pc.get_config_parameter('tmpDataFolder')
     returns a pathlib.Path object
     """
-    assert isinstance(cf.get_config_parameter('tmpDataFolder'), pathlib.Path)
+    assert isinstance(pc.get_config_parameter('tmpDataFolder'), pathlib.Path)

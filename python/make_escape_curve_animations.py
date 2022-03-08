a052680f269f98445846392aa0497748e5d2e4aa
piotr.idzik <vil02@o2.pl>
2022-03-08 17:41:21 +0100

    feat: add halfplane example

--- python/make_escape_curve_animations.py
+++ python/make_escape_curve_animations.py
@@ -20,6 +20,7 @@ def get_optimizer(in_angles_to_points_fun):
         def _check_data(cls):
             print(cls.max_curve_len, cls.data_size, cls.segment_size)
             assert cls.max_curve_len/cls.data_size == cls.segment_size
+            assert len(cls.bounds) == cls.data_size
 
         @classmethod
         def to_curve(cls, in_data):
@@ -43,12 +44,33 @@ def get_optimizer(in_angles_to_points_fun):
                 res_data.append(
                     RowType(in_data, in_fun_val, time.time()-start_time))
 
-            scipy.optimize.dual_annealing(
+            opt_res = scipy.optimize.dual_annealing(
                 lambda x: cls.evaluate_data(x),
                 cls.bounds,
                 maxiter=cls.maxiter,
                 callback=callback_fun)
+            res_data.append(
+                RowType(opt_res.x, opt_res.fun, time.time()-start_time))
             return res_data
+
+        @classmethod
+        def plot_curve(cls, curve_data, in_length, in_curve_color):
+            last_segment_num = int(in_length/curve_data.segment_size)
+            rem_len = in_length-last_segment_num*curve_data.segment_size
+            assert 0 <= rem_len < curve_data.segment_size
+            if len(curve_data.point_list) > last_segment_num:
+                plt.plot(
+                    curve_data.x_list[last_segment_num:],
+                    curve_data.y_list[last_segment_num:],
+                    color='lightgray')
+            pos_a = numpy.array(curve_data.point_list[last_segment_num])
+            pos_b = numpy.array(curve_data.point_list[last_segment_num+1])
+            pos_c = pos_a+(rem_len/curve_data.segment_size)*(pos_b-pos_a)
+            used_x_list = list(curve_data.x_list[0:last_segment_num+1]) + \
+                [pos_a[0], pos_c[0]]
+            used_y_list = list(curve_data.y_list[0:last_segment_num+1]) + \
+                [pos_a[1], pos_c[1]]
+            plt.plot(used_x_list, used_y_list, color=in_curve_color)
     return Optimizer
 
 
@@ -76,7 +98,8 @@ def get_example_with_single_shape(in_angles_to_points_fun):
             cur_curve = cls.to_curve(in_data)
             _init_figure()
             cls.shape.plot(facecolor='lightgreen', edgecolor='green')
-            plt.plot(cur_curve.x_list, cur_curve.y_list, color=curve_color)
+            cls.plot_curve(
+                cur_curve, cls.evaluate_curve(cur_curve), curve_color)
             cls._set_limits()
     return ExampleWithSingleShape
 
@@ -88,7 +111,7 @@ def get_unit_ball_example(in_angles_to_points_fun):
 
         @classmethod
         def _set_limits(_):
-            plot_size = 1.05
+            plot_size = 1.15
             plt.gca().set_xlim([-plot_size, plot_size])
             plt.gca().set_ylim([-plot_size, plot_size])
     return UnitBallExample
@@ -164,12 +187,18 @@ def get_example_with_multiple_shapes(in_angles_to_points_fun):
                 cls.bounds,
                 maxiter=cls.maxiter,
                 callback=callback_fun)
-            scipy.optimize.dual_annealing(
+            res_data.append(RowType(
+                res_sum.x, cls.evaluate_data_max(res_sum.x),
+                time.time()-start_time))
+            res_max = scipy.optimize.dual_annealing(
                 lambda x: cls.evaluate_data_max(x),
                 cls.bounds,
-                maxiter=10,
+                maxiter=40,
                 callback=callback_fun,
                 x0=res_sum.x)
+            res_data.append(RowType(
+                res_max.x, cls.evaluate_data_max(res_max.x),
+                time.time()-start_time))
             return res_data
 
         @classmethod
@@ -177,7 +206,8 @@ def get_example_with_multiple_shapes(in_angles_to_points_fun):
             cur_curve = cls.to_curve(in_data)
             _init_figure()
             #cls.shape.plot(facecolor='lightgreen', edgecolor='green')
-            plt.plot(cur_curve.x_list, cur_curve.y_list, color=curve_color)
+            cls.plot_curve(
+                cur_curve, cls.evaluate_curve_max(cur_curve, 10), curve_color)
             cls._set_limits()
     return ExampleWithMultipleShapes
 
@@ -185,7 +215,7 @@ def get_example_with_multiple_shapes(in_angles_to_points_fun):
 class Rectangle:
     def __init__(self, in_xy_data):
         self._xy_data = in_xy_data
-        self._patch_data = plt.Polygon(self.xy_data)
+        self._patch_data = plt.Polygon(self._xy_data)
 
     def __contains__(self, in_pos):
         return self._patch_data.contains_point(in_pos)
@@ -210,6 +240,22 @@ def _get_data_for_strip(in_shift_num, in_rotation_num):
     return res_list
 
 
+def _get_data_for_halfplane(in_rotation_num):
+    margin_size = 20
+    x_shift = -0.9
+    initial_xy_data = numpy.array(
+        [[-margin_size, margin_size], [margin_size, margin_size],
+         [margin_size, x_shift], [-margin_size, x_shift]])
+    res_list = []
+    for cur_rot in numpy.linspace(numpy.radians(0), numpy.radians(360), in_rotation_num, endpoint=False):
+        cur_xy = [rotations.rotate_2d(_, cur_rot) for _ in initial_xy_data]
+        cur_shape = Rectangle(cur_xy)
+        assert numpy.array([0, 0]) in cur_shape
+        res_list.append(cur_shape)
+    print(len(res_list))
+    return res_list
+
+
 def get_strip_example(in_angles_to_points_fun):
     class StripExample(
             get_example_with_multiple_shapes(in_angles_to_points_fun)):
@@ -221,6 +267,26 @@ def get_strip_example(in_angles_to_points_fun):
             plt.gca().set_xlim([-plot_size, plot_size])
             plt.gca().set_ylim([-plot_size, plot_size])
     return StripExample
+
+
+def get_halfplane_example(in_angles_to_points_fun):
+    class HalfplaneExample(
+            get_example_with_multiple_shapes(in_angles_to_points_fun)):
+        shapes = _get_data_for_halfplane(70)
+        maxiter = 250
+        data_size = 80
+        max_curve_len = 9
+        segment_size = max_curve_len/data_size
+        bounds = [
+            (numpy.radians(-180), numpy.radians(180)) for _ in range(data_size)]
+
+        @classmethod
+        def _set_limits(_):
+            plot_size = 3.1
+            plt.gca().set_xlim([-plot_size, plot_size])
+            plt.gca().set_ylim([-plot_size, plot_size])
+
+    return HalfplaneExample
 
 
 def generate_animation_data(
@@ -250,26 +316,36 @@ def generate_animation_data(
     return optimisation_data
 
 
+def _init_conv_plot():
+    plt.figure(figsize=(5, 1.8))
+
+
+def _plot_conv_data(in_data, in_color, in_label):
+    plt.plot(
+        [_.time for _ in in_data],
+        [_.function_value for _ in in_data],
+        color=in_color, label=in_label)
+
+
+def _save_tex_str_conv_plot(in_paths):
+    tex_str = \
+        f'\\includegraphics[width=\\textwidth]{{{in_paths.get_short_pdf_path(0)}}}\n'
+    with open(
+            in_paths.get_tex_file_path(), 'w', encoding='utf-8') as tex_file:
+        tex_file.write(tex_str)
+
+
 def create_comparison_conv_plot(in_logo_data, in_azimuth_data, in_paths):
-    def plot_data(in_data, in_color, in_label):
-        plt.plot(
-            [_.time for _ in in_data],
-            [_.function_value for _ in in_data],
-            color=in_color, label=in_label)
-    plt.figure(figsize=(5, 2.0))
-    plot_data(in_logo_data, ps.LOGO_COLOR, 'LOGO')
-    plot_data(in_azimuth_data, ps.AZIMUTH_COLOR, 'azymut')
+    _init_conv_plot()
+    _plot_conv_data(in_logo_data, ps.LOGO_COLOR, 'LOGO')
+    _plot_conv_data(in_azimuth_data, ps.AZIMUTH_COLOR, 'azymut')
     plt.legend(loc="upper right")
     plt.xlabel('[s]')
     plt.savefig(
         in_paths.get_pdf_file_path(0),
         bbox_inches='tight', pad_inches=0.01)
     plt.close()
-    tex_str = \
-        f'\\includegraphics[width=\\textwidth]{{{in_paths.get_short_pdf_path(0)}}}\n'
-    with open(
-            in_paths.get_tex_file_path(), 'w', encoding='utf-8') as tex_file:
-        tex_file.write(tex_str)
+    _save_tex_str_conv_plot(in_paths)
 
 
 def make_single_shape_plots(
@@ -290,6 +366,26 @@ def make_single_shape_plots(
     create_comparison_conv_plot(
         logo_data, azimuth_data,
         op.OutputPaths(conv_plot_core_name, conv_plot_tex_name))
+
+
+def make_multiple_shapes_plots(
+        in_example_class, core_name, tex_name,
+        conv_plot_name, conv_plot_tex_name):
+    opt_data = generate_animation_data(
+            in_example_class,
+            core_name, tex_name, ps.AZIMUTH_COLOR)
+
+    conv_paths = op.OutputPaths(conv_plot_name, conv_plot_tex_name)
+    _init_conv_plot()
+    _plot_conv_data(opt_data, ps.AZIMUTH_COLOR, 'azumut')
+    plt.xlabel('[s]')
+    y_limits = plt.gca().get_ylim()
+    plt.gca().set_ylim([y_limits[0], 8])
+    plt.savefig(
+        conv_paths.get_pdf_file_path(0),
+        bbox_inches='tight', pad_inches=0.01)
+    plt.close()
+    _save_tex_str_conv_plot(conv_paths)
 
 
 make_single_shape_plots(
@@ -316,3 +412,13 @@ make_single_shape_plots(
 generate_animation_data(
         get_strip_example(curve.angles_to_points_azimuth),
         'escape_from_strip', 'escapeFromStripTex', ps.AZIMUTH_COLOR)
+
+make_multiple_shapes_plots(
+        get_strip_example(curve.angles_to_points_azimuth),
+        'escape_from_strip', 'escapeFromStripTex',
+        'escape_from_strip_conv_plot', 'escapeFromStripConvPlotTex')
+
+make_multiple_shapes_plots(
+        get_halfplane_example(curve.angles_to_points_azimuth),
+        'escape_from_halfplane', 'escapeFromHalfplaneTex',
+        'escape_from_halfplane_conv_plot', 'escapeFromHalfplaneConvPlotTex')

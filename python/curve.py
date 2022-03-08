e8f3821f56a135cd181b03c318e2e963364a08cc
piotr.idzik <vil02@o2.pl>
2022-03-08 20:54:14 +0100

    feat: add PointCurve

--- python/curve.py
+++ python/curve.py
@@ -49,35 +49,43 @@ def logo_agnles_to_azimuth_angles(in_logo_angles):
     return res
 
 
+class _AbstractCurve:
+    @property
+    def point_list(self):
+        """returns potins of the curve in order"""
+        return self._point_list
+
+    @property
+    def x_list(self):
+        """returns the x-coordinates of point_list"""
+        return tuple(_[0] for _ in self.point_list)
+
+    @property
+    def y_list(self):
+        """returns the y-coordinates of point_list"""
+        return tuple(_[1] for _ in self.point_list)
+
+    @property
+    def angle_list(self):
+        """returns the angle_list"""
+        return self._angle_list
+
+
 def get_angle_curve_class(in_to_point_list_fun):
     """returns an AngleCurve class"""
-    class AngleCurve:
+    class AngleCurve(_AbstractCurve):
         """
         represents a curve
         """
         def __init__(self, in_angle_list, in_segment_size):
             self._angle_list = copy.deepcopy(in_angle_list)
-            self._segment_size = in_segment_size
             self._point_list = in_to_point_list_fun(
-                self._angle_list, self._segment_size)
+                self._angle_list, in_segment_size)
 
-        @property
-        def point_list(self):
-            """returns potins of the curve in order"""
-            return self._point_list
-
-        @property
-        def x_list(self):
-            """returns the x-coordinates of point_list"""
-            return tuple(_[0] for _ in self.point_list)
-
-        @property
-        def y_list(self):
-            """returns the y-coordinates of point_list"""
-            return tuple(_[1] for _ in self.point_list)
-
-        @property
-        def angle_list(self):
-            """returns the angle_list"""
-            return self._angle_list
     return AngleCurve
+
+
+class PointCurve(_AbstractCurve):
+    """represetns a zig-zag-curvestarting at the point (0, 0)"""
+    def __init__(self, in_point_list):
+        self._point_list = [(0, 0)]+in_point_list

bcdad76315ab33e14c4cd89138eaa2d327cfbaaa
piotr.idzik <vil02@o2.pl>
2022-03-10 00:31:37 +0100

    feat:  add make_escape_from_halfplane_ex.py

---
+++ python/figure_saver.py
@@ -0,0 +1,27 @@
+"""contains the definition of the class FigureSaver"""
+
+import matplotlib.pyplot as plt
+
+
+class FigureSaver:
+    """utility class to hande saving figure in sequences"""
+    _cur_fig_num = 0
+
+    def __init__(self, output_paths):
+        self._output_paths = output_paths
+
+    def save_fig(self):
+        """saves the current figre and increases the counter"""
+        def _set_limits():
+            plt.gca().set_xlim([-13, 13])
+            plt.gca().set_ylim([-7.5, 7.5])
+        _set_limits()
+        plt.savefig(
+            self._output_paths.get_pdf_file_path(self.fig_num),
+            bbox_inches='tight', pad_inches=0.01)
+        self._cur_fig_num += 1
+
+    @property
+    def fig_num(self):
+        """returns the current figure number"""
+        return self._cur_fig_num
---
+++ python/make_escape_from_halfplane_ex.py
@@ -0,0 +1,89 @@
+"""
+generates TeX data for escape from halfplane explanation
+"""
+import matplotlib.pyplot as plt
+import matplotlib.transforms
+import numpy
+
+import output_paths as op
+import project_styles as ps
+import figure_saver as fs
+
+OUTPUT_PATHS = op.OutputPaths('escapeFromHalfplaneExTex')
+FIGURE_SAVER = fs.FigureSaver(OUTPUT_PATHS)
+
+
+def _init_figure():
+    plt.close()
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.axis('off')
+
+
+def _draw_halfplane():
+    plt.gca().add_patch(plt.Rectangle((-50, -1), 100, 101, **ps.CONVEX_COLORS))
+
+
+def _get_rectangle(**kwargs):
+    return plt.Rectangle((-4, -1), 8, 7, **kwargs)
+
+
+def _draw_rectangle():
+    plt.gca().add_patch(_get_rectangle(**ps.CONVEX_COLORS))
+
+
+def _mark_pos():
+    plt.plot(0, 0, marker='o', color='black', markersize=8)
+
+
+_init_figure()
+_draw_halfplane()
+FIGURE_SAVER.save_fig()
+plt.plot([-50, 50], [0, 0], color='black', linestyle='--')
+FIGURE_SAVER.save_fig()
+
+_init_figure()
+_draw_rectangle()
+_mark_pos()
+FIGURE_SAVER.save_fig()
+
+ANGLE_LIST = numpy.linspace(0, 2*numpy.pi, 5, False)
+
+for _ in ANGLE_LIST:
+    arrow_len = 1.7
+    cur_x = arrow_len*numpy.cos(_)
+    cur_y = arrow_len*numpy.sin(_)
+    plt.arrow(0, 0, cur_x, cur_y, overhang=0.3, head_width=0.5, color='black')
+FIGURE_SAVER.save_fig()
+
+_init_figure()
+COLOR_LIST = ['teal', 'maroon', 'blue', 'magenta', 'crimson']
+for (cur_angle, cur_color) in zip(ANGLE_LIST, COLOR_LIST):
+    cur_patch = _get_rectangle(color=cur_color, alpha=0.3)
+    cur_rot = \
+        matplotlib.transforms.Affine2D().rotate(cur_angle)+plt.gca().transData
+    cur_patch.set_transform(cur_rot)
+
+    plt.gca().add_patch(cur_patch)
+_mark_pos()
+FIGURE_SAVER.save_fig()
+plt.close()
+
+TEX_STR = \
+    '\\begin{frame}\n' \
+    '  \\begin{center}\n' \
+    '    \\begin{overprint}\n'
+
+for _ in range(FIGURE_SAVER.fig_num):
+    cur_str = \
+        f'        \\onslide<{_+1}>' \
+        r'\centerline{\includegraphics[width=0.9\textwidth]{' \
+        f'{OUTPUT_PATHS.get_short_pdf_path(_)}' \
+        '}}\n'
+    TEX_STR += cur_str
+TEX_STR += \
+    '    \\end{overprint}\n' \
+    '  \\end{center}\n'
+TEX_STR += '\\end{frame}\n'
+
+with open(OUTPUT_PATHS.get_tex_file_path(), 'w', encoding='utf-8') as tex_file:
+    tex_file.write(TEX_STR)

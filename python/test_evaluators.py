d5269e91db381b160feba63b81c8f35afef4282f
piotr.idzik <vil02@o2.pl>
2022-03-14 17:59:04 +0100

    refctor: change cost_functions.py into evaluators.py

--- python/test_evaluators.py
+++ python/test_evaluators.py
@@ -1,7 +1,7 @@
 """tests for the most cost_functions"""
 import pytest
 
-import cost_functions as cf
+import evaluators as ev
 import curve_representations as cr
 import convex_shapes
 
@@ -25,13 +25,12 @@ def _get_trivial_data(in_curve_representation):
 
 @pytest.mark.parametrize(
     "example_representation", _get_example_representations())
-def test_get_single_shape_cost(example_representation):
-    """test of the function get_single_shape_cost"""
-    example_shape = convex_shapes.Wheel((0, 0, ), 1)
-    cur_cost_fun = cf.get_single_shape_cost(
-        example_representation, example_shape, 20)
+def test_evaluate_data_in_single_shape(example_representation):
+    """test of the method evaluate_data"""
+    cur_evaluator = ev.get_single_shape_evaluator(
+        example_representation, convex_shapes.Wheel((0, 0, ), 1), 20)
     cur_data = _get_trivial_data(example_representation)
-    assert abs(cur_cost_fun(cur_data)-1) < 0.000001
+    assert abs(cur_evaluator.evaluate_data(cur_data)-1) < 0.000001
 
 
 def _get_example_shape_list():
@@ -40,21 +39,19 @@ def _get_example_shape_list():
 
 @pytest.mark.parametrize(
     "example_representation", _get_example_representations())
-def test_get_cost_max(example_representation):
-    """test of the function get_cost_max"""
-    example_shape_list = _get_example_shape_list()
-    cur_cost_fun = cf.get_cost_max(
-        example_representation, example_shape_list, 20)
+def test_evaluate_data_max_in_multiple_shape(example_representation):
+    """test of the method evaluate_data"""
+    cur_evaluator = ev.get_multiple_shape_evaluator(
+        example_representation, _get_example_shape_list(), 20)
     cur_data = _get_trivial_data(example_representation)
-    assert abs(cur_cost_fun(cur_data)-2) < 0.000001
+    assert abs(cur_evaluator.evaluate_data_max(cur_data)-2) < 0.000001
 
 
 @pytest.mark.parametrize(
     "example_representation", _get_example_representations())
-def test_get_cost_sum(example_representation):
-    """test of the function get_cost_sum"""
-    example_shape_list = _get_example_shape_list()
-    cur_cost_fun = cf.get_cost_sum(
-        example_representation, example_shape_list, 20)
+def test_evaluate_data_sum_in_multiple_shape(example_representation):
+    """test of the method evaluate_data"""
+    cur_evaluator = ev.get_multiple_shape_evaluator(
+        example_representation, _get_example_shape_list(), 20)
     cur_data = _get_trivial_data(example_representation)
-    assert abs(cur_cost_fun(cur_data)-3) < 0.000001
+    assert abs(cur_evaluator.evaluate_data_sum(cur_data)-3) < 0.000001
